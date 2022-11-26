e162dde699466294e1b01dd62c53f8a0e925e451
MiguelCriollo <macriollo.2@sudamericano.edu.ec>
2022-11-26 10:59:28 -0500

    Se agrega nuevo archivos
    Estos no estan relacionados con Multiplicador de matrices

--- ejercicios.py
+++ ejercicios.py
@@ -1,6 +1,22 @@
 
-def run():
+def read():
+    lista_Generada=[]
+    
+    with open("./palabras.txt", "r", encoding="utf-8") as f:
+        lista_Generada=f.read()
+    f.close()
+    #for x in lista_Generada:
+    #    print(x)
+    #    if x=="\n":
+    #        print("Validado")
+    valoresBlancos=[x for x in lista_Generada if x==" "]
+    print(valoresBlancos)
+def separarPalabras():
     pass
+        
+
+def run():
+    read()
 
 if __name__== '__main__':
     run()
