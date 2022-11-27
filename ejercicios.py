3b4514aeef0f58699b3c5cc3eebc881a47cd7017
MiguelCriollo <macriollo.2@sudamericano.edu.ec>
2022-11-27 12:11:02 -0500

    Validacion letra y primera parte terminada

--- ejercicios.py
+++ ejercicios.py
@@ -6,28 +6,55 @@ def read():
     f.close()
     return listaPalabras
 
+def validacionLetra(palabraEscondidaList):
+    while True:
+        letraIngresada=input("Ingrese letra")
+        #mostrarPuzzle(palabraEscondidaList)
+        try:
+            if len(letraIngresada)==1:
+                letraIngresada=int(letraIngresada)  
+        except:
+            print("Letra correcta")
+            return letraIngresada
+        else:
+            print("No ingrese = (Numeros, no mas de dos letras)")
+            
+    
+
 
 
 
 def generar_Palabra(lista):
-    palabraGenerada=choice(lista)
-    return palabraGenerada
+    return choice(lista)
 
+def mostrarPuzzle(palabraEscondidaList):
+    print(" ".join(palabraEscondidaList))
 
 
 def run():
     listaPalabras=read()
+
     palabraGenerada=generar_Palabra(listaPalabras)
     palabraEscondidaList=list("_"*len(palabraGenerada))
-    while True:
-        letraIngresada=input("Ingrese letra")
+    print(palabraGenerada)
+
+    
+
+                
+    while True: 
+        letraIngresada=validacionLetra(palabraEscondidaList)
         for x ,y in enumerate(palabraGenerada):
             print(f"La posicion de x es={x} y el valor de y es {y}")
             if y==letraIngresada:
                 palabraEscondidaList[x]=y
                 
+        #mostrarPuzzle(palabraEscondidaList)        
 
         print(" ".join(palabraEscondidaList))
+        #Cuando se completen todas las palabras restantes:
+        if "_" not in palabraEscondidaList:
+            print("Acabado")
+            break
             
             
 
